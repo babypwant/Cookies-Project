36fb0621e7ac3b7df60db26f9b5a83eaf6b25bad
gary <baby_plant@icloud.com>
2021-03-29 16:17:05 -0700

    Step 6

--- cookies-project/public/js/cookies.js
+++ cookies-project/public/js/cookies.js
@@ -8,13 +8,43 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.cookie = `${name}=${value}`;
   };
 
-   setCookie('monster', 'rabbit');
+  setCookie('monster', 'rabbit');
 
 
   const getCookie = () => {
     let cookies = document.cookie
-  let list = cookies.split('; ')
-  return list
+    let list = cookies.split('; ')
+    return list
   }
   console.log(getCookie())
+
+  const getCookieValue = (name) => {
+  
+    let list =  getCookie()
+    // for(i)
+    // let currentEl = list[i]
+    // iscurrenEl.includes(anem)
+    // let monsteranme = currentEl.split('=')
+    if (list.includes(name)) {
+      
+      // let value = document.cookie.name.split('=')
+      for (cookies in document.cookie) {
+        console.log(cookies)
+        if (cookies.includes(name)) {
+
+          return cookies
+        }
+
+      }
+
+    }
+
+    if (document.cookie === undefined) {
+      return null;
+    }
+
+
+
+  }
+  console.log(getCookieValue('monster'))
 });
